149355b7c135156cb1e9c8b8cefe6168bb57d0a4
Ervin Pangilinan <pangilinanervin22@gmail.com>
2022-03-26 13:57:48 +0800

    add searching algorithm

---
+++ Main.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+import SearchingAlgorithm.SearchingAlgorithm;
+
+public class Main {
+
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        boolean findValue;
+
+        int[] unSortArray = { 4, 1, 3, 5, 2, 7, 9, 6, 10, 8 };
+        int[] sortArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        // * best way to find a value in a unsorted array
+        // * TIME = best 0(1) / worst o(n)
+        findValue = SearchingAlgorithm.linerSearch(unSortArray, 5);
+        System.out.println(findValue);
+
+        // ! array must be sorted
+        // * Time o(log n)
+        findValue = SearchingAlgorithm.binarySearchIterative(sortArray, 5);
+        System.out.println(findValue);
+    }
+
+}
---
+++ SearchingAlgorithm/SearchingAlgorithm.java
@@ -0,0 +1,49 @@
+package SearchingAlgorithm;
+
+public class SearchingAlgorithm {
+    public static boolean linerSearch(int[] array, int search) {
+        for (int num : array)
+            if (search == num)
+                return true;
+        return false;
+    }
+
+    public static boolean binarySearchIterative(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int middle = (left + right) / 2;
+
+            if (array[middle] == target || array[right] == target || array[left] == target)
+                return true;
+
+            if (target < array[middle])
+                right = middle - 1;
+            else
+                left = middle + 1;
+        }
+        return false;
+    }
+
+    public static boolean binarySearchRec(int[] array, int target) {
+        return binarySearchRec(array, target, 0, array.length - 1);
+    }
+
+    private static boolean binarySearchRec(int[] array, int target, int left, int right) {
+        if (right <= left)
+            return false;
+
+        System.out.println("1");
+        int middle = (left + right) / 2;
+
+        if (array[middle] == target)
+            return true;
+
+        if (target < array[middle])
+            return binarySearchRec(array, target, left, middle - 1);
+
+        return binarySearchRec(array, target, middle + 1, right);
+    }
+
+}
