91376b044a09e9a71a14a44a523eac26bb9b7f76
Ervin Pangilinan <pangilinanervin22@gmail.com>
2022-05-14 22:45:50 +0800

    add priority queue

---
+++ DataStructures/PriorityQueueScratch.java
@@ -0,0 +1,56 @@
+package DataStructures;
+
+import java.util.Arrays;
+
+public class PriorityQueueScratch {
+    private int[] items = new int[5];
+    private int count;
+
+    public void add(int input) {
+        // resize the array
+        if (count == items.length)
+            resize();
+
+        // shifting items
+        int i;
+        for (i = count - 1; i >= 0; i--) {
+            if (items[i] > input) {
+                items[i + 1] = items[i];
+            } else
+                break;
+        }
+        items[i + 1] = input;
+        count++;
+    }
+
+    public int remove() {
+        if (isEmpty())
+            System.out.println("nothing to remove");
+
+        return items[--count];
+    }
+
+    //
+    // public int peek() {
+    // }
+
+    public void resize() {
+        var tempArray = new int[items.length * 2];
+        var current = 0;
+        for (int num : items) {
+            tempArray[current] = num;
+            current++;
+        }
+        items = tempArray;
+    }
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(items);
+    }
+
+}
